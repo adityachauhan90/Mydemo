7971051b7251d975336f95c3adcc216fc9a54de9
adityachauhan90 <adityachauhan901277@gmail.com>
2025-04-29 08:41:15 +0530

    done

--- src/Web.jsx
+++ src/Web.jsx
@@ -4,6 +4,7 @@ function Web() {
   return (
     <div>
         <h1>Hello</h1>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, facilis quo minus praesentium labore necessitatibus harum, quam repellat nisi quasi sequi voluptas ex voluptate eaque molestiae quibusdam quae modi? Molestiae.</p>
     </div>
   )
 }
